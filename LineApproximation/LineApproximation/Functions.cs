9917b4e417ef318bab82d9bb22d90de6244cb000
Richard Tia <richie.tia@gmail.com>
2013-03-03 16:07:29 -0500

    Implementation of Douglas-Pecker Line approximation

---
+++ LineApproximation/LineApproximation/Functions.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace LineApproximation
+{
+    class Functions
+    {
+        /// <summary>
+        /// Uses the Douglas Peucker algorithm to reduce the number of points.
+        /// </summary>
+        /// <param name="points">The points.</param>
+        /// <param name="tolerance">The tolerance.</param>
+        /// <returns></returns>
+        public static List<Point> DouglasPeuckerReduction
+            (List<Point> points, Double tolerance)
+        {
+            if (points == null || points.Count < 3)
+                return points;
+
+            Int32 firstPoint = 0;
+            Int32 lastPoint = points.Count - 1;
+            List<Int32> pointIndexsToKeep = new List<Int32>();
+
+            //Add the first and last index to the keepers
+            pointIndexsToKeep.Add(firstPoint);
+            pointIndexsToKeep.Add(lastPoint);
+
+            //The first and the last point cannot be the same
+            while (points[firstPoint].Equals(points[lastPoint]))
+            {
+                lastPoint--;
+                //might need this following line
+                //pointIndexsToKeep.Add(lastPoint);
+            }
+
+            DouglasPeuckerReduction(points, firstPoint, lastPoint,
+            tolerance, ref pointIndexsToKeep);
+
+            List<Point> returnPoints = new List<Point>();
+            pointIndexsToKeep.Sort();
+            foreach (Int32 index in pointIndexsToKeep)
+            {
+                returnPoints.Add(points[index]);
+            }
+
+            return returnPoints;
+        }
+
+        /// <summary>
+        /// Douglases the peucker reduction.
+        /// </summary>
+        /// <param name="points">The points.</param>
+        /// <param name="firstPoint">The first point.</param>
+        /// <param name="lastPoint">The last point.</param>
+        /// <param name="tolerance">The tolerance.</param>
+        /// <param name="pointIndexsToKeep">The point index to keep.</param>
+        private static void DouglasPeuckerReduction(List<Point>
+            points, Int32 firstPoint, Int32 lastPoint, Double tolerance,
+            ref List<Int32> pointIndexsToKeep)
+        {
+            Double maxDistance = 0;
+            Int32 indexFarthest = 0;
+
+            for (Int32 index = firstPoint; index < lastPoint; index++)
+            {
+                Double distance = PerpendicularDistance
+                    (points[firstPoint], points[lastPoint], points[index]);
+                if (distance > maxDistance)
+                {
+                    maxDistance = distance;
+                    indexFarthest = index;
+                }
+            }
+
+            if (maxDistance > tolerance && indexFarthest != 0)
+            {
+                //Add the largest point that exceeds the tolerance
+                pointIndexsToKeep.Add(indexFarthest);
+
+                DouglasPeuckerReduction(points, firstPoint,
+                indexFarthest, tolerance, ref pointIndexsToKeep);
+                DouglasPeuckerReduction(points, indexFarthest,
+                lastPoint, tolerance, ref pointIndexsToKeep);
+            }
+        }
+
+        /// <summary>
+        /// The distance of a point from a line made from point1 and point2.
+        /// </summary>
+        /// <param name="pt1">The PT1.</param>
+        /// <param name="pt2">The PT2.</param>
+        /// <param name="p">The p.</param>
+        /// <returns></returns>
+        public static Double PerpendicularDistance
+            (Point Point1, Point Point2, Point Point)
+        {
+            //Area = |(1/2)(x1y2 + x2y3 + x3y1 - x2y1 - x3y2 - x1y3)|   *Area of triangle
+            //Base = v((x1-x2)²+(x1-x2)²)                               *Base of Triangle*
+            //Area = .5*Base*H                                          *Solve for height
+            //Height = Area/.5/Base
+
+            //Calculation for area of a triangle
+            Double triangleArea = Math.Abs(.5 * (Point1.X * Point2.Y + 
+                                    Point2.X * Point.Y + 
+                                    Point.X * Point1.Y - 
+                                    Point2.X * Point1.Y - 
+                                    Point.X * Point2.Y - 
+                                    Point1.X * Point.Y));
+
+            //Calculation for base of a triangle
+            Double triangleBase = Math.Sqrt(Math.Pow(Point1.X - Point2.X, 2) +
+                            Math.Pow(Point1.Y - Point2.Y, 2));
+
+            //Calculation for height of a triable (given 
+            Double triangleHeight = triangleArea / triangleBase * 2;
+
+            return triangleHeight;
+        }
+    }
+}
